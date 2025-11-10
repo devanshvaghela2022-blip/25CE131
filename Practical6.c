5cce3667784385f485f040f77b1e87b3111bbddf
25CE131 <devanshvaghela2022@gmail.com>
2025-11-10 21:29:31 +0530

    Update Practical6.c

--- Practical6.c
+++ Practical6.c
@@ -2,9 +2,13 @@
 int main()
 {
     float C,F;
+    printf("\n Name :- Devansh Vaghela");
+    printf("\n Id no. :- 25CE131");
+    printf("\n Batch & Division :- C2");
     printf("\n Enter value of temperature in degree celsius := ");
     scanf("%f",&C);
     F=(C*9/5)+32;
     printf("\n Temperature in Farhenheit := %f ",F);
     return 0;
 }
+
