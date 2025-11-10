d4b3369db810f78344e24a83775ddce240bc9000
25CE131 <devanshvaghela2022@gmail.com>
2025-11-10 21:44:34 +0530

    Add files via upload

---
+++ Practical30.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j,flag=0;
+    printf("\n Enter the no. of items :- ");
+    scanf("%d",&n);
+    if(n>0)
+    {
+        float arr[n];
+        float *inventory,*inventory1,temp=0;
+        inventory=arr;
+        for(i=0; i<n; i++)
+        {
+            printf("\n Enter the price of %d item :- ",i+1);
+            if(scanf("%f",inventory)!=1)
+            {
+                flag=1;
+                fflush(stdin);
+            }
+            inventory++;
+        }
+        if(flag==1)
+        {
+            printf("\n Invalid Input. Items can't be sorted.");
+            return 1;
+        }
+        for(i=0; i<n; i++)
+        {
+            inventory=&arr[i];
+            for(j=i+1; j<n; j++)
+            {
+                inventory1=&arr[j];
+                if(*inventory > *inventory1)
+                {
+                    *inventory=*inventory1;
+                    temp=*inventory;
+                    *inventory1=temp;
+                }
+            }
+            for(i=0; i<n; i++)
+            {
+                printf("\n Sorted item prices :- %f",*inventory);
+                inventory++;
+            }
+        }
+    }
+    if(n==0)
+    {
+        printf("\n No items to be sorted.");
+    }
+    return 0;
+}
