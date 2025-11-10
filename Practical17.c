1450029ce8ed058a58112c26d379fd790e84cc54
25CE131 <devanshvaghela2022@gmail.com>
2025-11-10 21:34:02 +0530

    Update Practical17.c

--- Practical17.c
+++ Practical17.c
@@ -3,6 +3,9 @@
 int main()
 {
     int i,min=0;
+    printf("\n Name :- Devansh Vaghela");
+    printf("\n Id no. :- 25CE131");
+    printf("\n Batch & Division :- C2");
     for(i=1; i<=50; i++)
     {
         printf("\n Minute %d : Distance Covered := %.1f km",i,0.5*i);
@@ -15,4 +18,5 @@ int main()
     }
     return 0;
 }
+
 
