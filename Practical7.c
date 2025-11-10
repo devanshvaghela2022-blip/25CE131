172e7d8e2a8b46f4346de577f3d7e93ed7bd8110
25CE131 <devanshvaghela2022@gmail.com>
2025-11-10 21:29:46 +0530

    Include user details in Practical7.c
    Added user information display before age prompt.

--- Practical7.c
+++ Practical7.c
@@ -2,6 +2,9 @@
 int main()
 {
     int age;
+    printf("\n Name :- Devansh Vaghela");
+    printf("\n Id no. :- 25CE131");
+    printf("\n Batch & Division :- C2");
     printf("\n Enter Age (0-120) := ");
     scanf("%d",&age);
     if(age<=12 && age>=0)
@@ -22,3 +25,4 @@ int main()
     }
     return 0;
 }
+
